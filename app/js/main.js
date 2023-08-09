6e35be4dfc6de005f2a54f55bd00169168842351
Nam Duong <duongquocnam224400@gmail.com>
2023-08-09 18:33:54 +0700

    Done team or reviewer part and responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+$(document).ready(function () {
+	$('.slider').slick({
+		centerMode: true,
+		dots: true,
+		autoplay: true,
+		speed: 300,
+		centerPadding: '50px',
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '30px',
+					Infinity: true,
+					slidesToShow: 1,
+				},
+			},
+		],
+	});
+});
